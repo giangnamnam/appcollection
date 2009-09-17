3c0f35adad1dc236fd65d98b8c3425e4eb5a02d4
shenbinsc <shenbinsc@cbf8b9f2-3a65-11de-be05-5f7a86268029>
2009-09-17 03:31:07 +0000

    预处理一张后调用搜脸库, 平均利用cpu

---
+++ branches/RemoteImaging_serialprocess/RemoteImaging/Query/ImageDirSys.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RemoteImaging.Query
+{
+    public class ImageDirSys
+    {
+        public static string BeginDir = Properties.Settings.Default.OutputPath;
+        public static string BigIconPath = Properties.Settings.Default.BigImageDirectoryName;
+        public static string IconPath = Properties.Settings.Default.IconDirectoryName;
+        public static string VideoPath = Properties.Settings.Default.VideoDirectoryName;
+        public enum SearchType { PicType, VideoType };
+
+        public ImageDirSys(string camera, string year, string month, string day, string hour, string minute, string second)
+        {
+            this.CameraID = camera;
+            this.Year = year;
+            this.Month = month;
+            this.Day = day;
+            this.Hour = hour;
+            this.Minute = minute;
+            this.Second = second;
+        }
+
+        public string CameraID
+        {
+            get;
+            set;
+        }
+        public string Year
+        {
+            get;
+            set;
+        }
+        public string Month
+        {
+            get;
+            set;
+        }
+        public string Day
+        {
+            get;
+            set;
+        }
+        public string Hour
+        {
+            get;
+            set;
+        }
+        public string Minute
+        {
+            get;
+            set;
+        }
+        public string Second
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ branches/RemoteImaging_serialprocess/RemoteImaging/VideoSearch.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace RemoteImaging
+{
+    using Core;
+
+    public static class VideoSearch
+    {
+        public static string[] FindVideos(string rootFolder, ImageDetail img)
+        {
+            DateTime utcTime = img.CaptureTime.ToUniversalTime();
+            string relPath = BuildRelativePathForVideoFile(utcTime);
+
+            string videoFilePath = Path.Combine(rootFolder, relPath);
+            if (File.Exists(videoFilePath))
+            {
+                string[] videos = new string[] { videoFilePath };
+                return videos;
+            }
+            else
+            {
+                return new string[0];
+            }
+        }
+
+        public static string[] FindVideos(int cameraID, DateTime startLocalTime, DateTime endLocalTime)
+        {
+            string rootFolder = Path.Combine(Properties.Settings.Default.OutputPath, cameraID.ToString("D2"));
+
+            DateTime startUTC = startLocalTime.ToUniversalTime();
+            DateTime endUTC = endLocalTime.ToUniversalTime();
+            List<string> files = new List<string>();
+
+            while (startUTC <= endUTC)
+            {
+                string relativePath = BuildRelativePathForVideoFile(startUTC);
+                string path = Path.Combine(rootFolder, relativePath);
+                if (File.Exists(path))
+                {
+                    files.Add(path);
+                }
+
+                startUTC = startUTC.AddMinutes(1);
+
+            }
+
+            return files.ToArray();
+
+        }
+
+        private static string BuildRelativePathForVideoFile(DateTime utcTime)
+        {
+            string relativePath = string.Format(@"NORMAL\{0:D4}{1:D2}{2:D2}\{3:D2}\{4:D2}.m4v",
+                            utcTime.Year, utcTime.Month, utcTime.Day, utcTime.Hour, utcTime.Minute);
+            return relativePath;
+        }
+    }
+}
