0b13231f32b911c89656c9097717485455557fb3
shenbinsc <shenbinsc@cbf8b9f2-3a65-11de-be05-5f7a86268029>
2009-09-23 06:14:34 +0000

    当图片太多的时候,结果显示太慢

---
+++ branches/RemoteImaging_shen_PicSearchSlow/FaceSelDll/FaceSel.cpp
@@ -0,0 +1,58 @@
+#include "FaceSelect.h"
+#include "FaceSel.h"
+
+
+static CFaceSelect g_FaceSelect;
+
+void SetLightMode(int iMode)
+{
+	g_FaceSelect.SetLightMode( iMode );
+}
+
+void AddInImage( IplImage* pImg, CvRect roi )
+{
+	g_FaceSelect.AddInImage( pImg, roi );
+}
+
+char* SelectBestImage()
+{
+	return g_FaceSelect.SelectBestImage( );
+}
+
+void SetROI( int x, int y, int width, int height )
+{
+	g_FaceSelect.SetROI( x, y, width, height );
+}
+void SetFaceParas( int iMinFace, double dFaceChangeRatio )
+{
+	g_FaceSelect.SetFaceParas( iMinFace, dFaceChangeRatio );
+}
+void SetDwSmpRatio( double dRatio )
+{
+	g_FaceSelect.SetDwSmpRatio( dRatio );
+}
+void SetOutputDir( const char* dir )
+{
+	g_FaceSelect.SetOutputDir( dir );
+}
+void SetExRatio( double topExRatio, double bottomExRatio, double leftExRatio, double rightExRatio )
+{
+	g_FaceSelect.SetExRatio( topExRatio, bottomExRatio, leftExRatio, rightExRatio );
+}
+
+
+
+
+/////////////////////////////////20090716 newly defined/////////////////////////////////////////
+void AddInFrame(Frame frame)//依次添加一组图片
+{
+	g_FaceSelect.AddInFrame( frame );
+}
+int SearchFaces(Target** targets)//一组添加完后，调用这个函数，返回脸数目
+{
+	return g_FaceSelect.SearchFaces( targets );
+}
+void ReleaseMem()
+{
+	g_FaceSelect.ReleaseTargets();
+}
---
+++ branches/RemoteImaging_shen_PicSearchSlow/RemoteImaging/MotionDetect/MotionDetect.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using ImageProcess;
+
+namespace MotionDetect
+{
+    public static class MotionDetect
+    {
+        const string dllName = "PreProcess.dll";
+
+        [DllImport(dllName, EntryPoint = "PreProcessFrame")]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static extern bool PreProcessFrame(Frame frame, ref Frame lastFrame);
+
+        [DllImport(dllName)]
+        public static extern void SetDrawRect(bool draw);
+
+        [DllImport(dllName)]
+        public static extern void SetAlarmArea(int leftX, int leftY, int rightX, int rightY, bool draw);
+
+        [DllImport(dllName)]
+        public static extern void SetRectThr(int fCount, int gCount);
+
+    }
+}
