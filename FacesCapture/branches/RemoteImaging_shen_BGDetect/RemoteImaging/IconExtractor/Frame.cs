fff78486c8a26aafccfed6a14683554734f7b405
shenbinsc <shenbinsc@cbf8b9f2-3a65-11de-be05-5f7a86268029>
2009-09-26 09:32:39 +0000

    添加背景检测功能

---
+++ FacesCapture/branches/RemoteImaging_shen_BGDetect/RemoteImaging/IconExtractor/Frame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using OpenCvSharp;
+
+namespace ImageProcess
+{
+    [StructLayoutAttribute(LayoutKind.Sequential)]
+    public struct Frame
+    {
+        public byte cameraID;
+        /// IplImage*
+        public System.IntPtr image;
+
+        public CvRect searchRect;
+
+        /// int
+        public long timeStamp;
+    }
+}
