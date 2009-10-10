e256d69dacd7b0c66c1041203b33b762eaa2b39f
shenbinsc <shenbinsc@cbf8b9f2-3a65-11de-be05-5f7a86268029>
2009-10-10 03:13:06 +0000

    微调 keygen 界面

--- FacesCapture/trunk/RemoteImaging/KeyGen/Form1.Designer.cs
+++ FacesCapture/trunk/RemoteImaging/KeyGen/Form1.Designer.cs
@@ -31,21 +31,23 @@
             this.textBoxSN = new System.Windows.Forms.TextBox();
             this.textBoxKey = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBoxSN
             // 
-            this.textBoxSN.Location = new System.Drawing.Point(126, 37);
+            this.textBoxSN.Location = new System.Drawing.Point(57, 29);
             this.textBoxSN.Name = "textBoxSN";
-            this.textBoxSN.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSN.Size = new System.Drawing.Size(370, 20);
             this.textBoxSN.TabIndex = 0;
             // 
             // textBoxKey
             // 
-            this.textBoxKey.Location = new System.Drawing.Point(126, 89);
+            this.textBoxKey.Location = new System.Drawing.Point(57, 73);
             this.textBoxKey.Multiline = true;
             this.textBoxKey.Name = "textBoxKey";
-            this.textBoxKey.Size = new System.Drawing.Size(276, 89);
+            this.textBoxKey.Size = new System.Drawing.Size(370, 126);
             this.textBoxKey.TabIndex = 1;
             // 
             // button1
@@ -58,11 +60,31 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "SN:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Key:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(498, 270);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBoxKey);
             this.Controls.Add(this.textBoxSN);
@@ -78,6 +100,8 @@
         private System.Windows.Forms.TextBox textBoxSN;
         private System.Windows.Forms.TextBox textBoxKey;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
