37c6bfa02761f48a6a9aba97c5151ed83bc79cb8
shenbinsc <shenbinsc@cbf8b9f2-3a65-11de-be05-5f7a86268029>
2009-11-09 03:18:58 +0000

    to add face compare feature

---
+++ FacesCapture/branches/FaceCompare/RemoteImaging/RemoteImaging/ManagedTarget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ImageProcess;
+
+namespace RemoteImaging
+{
+    class ManagedTarget
+    {
+        /// Frame
+        public Frame BaseFrame;
+
+        public Face[] Faces;
+    }
+}
