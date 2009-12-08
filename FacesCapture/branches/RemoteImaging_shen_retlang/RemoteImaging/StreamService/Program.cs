28440487c87d80129aab938e4243f855f2428389
shenbinsc <shenbinsc@cbf8b9f2-3a65-11de-be05-5f7a86268029>
2009-12-08 08:25:17 +0000

    改视频播放为单独的系统服务

--- FacesCapture/branches/RemoteImaging_shen_retlang/RemoteImaging/StreamService/Program.cs
+++ FacesCapture/branches/RemoteImaging_shen_retlang/RemoteImaging/StreamService/Program.cs
@@ -1,22 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Forms;
+using System.ServiceProcess;
+using System.Text;
 
-namespace StreamServer
+namespace StreamService
 {
     static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
         static void Main()
         {
-            
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new StreamServer());
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+			{ 
+				new StreamingService() 
+			};
+            ServiceBase.Run(ServicesToRun);
         }
     }
 }
--- FacesCapture/branches/RemoteImaging_shen_retlang/RemoteImaging/StreamService/Properties/AssemblyInfo.cs
+++ FacesCapture/branches/RemoteImaging_shen_retlang/RemoteImaging/StreamService/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StreamServer")]
+[assembly: AssemblyTitle("StreamService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("China")]
-[assembly: AssemblyProduct("StreamServer")]
+[assembly: AssemblyProduct("StreamService")]
 [assembly: AssemblyCopyright("Copyright © China 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0ba72f52-6dee-4267-a2f1-22b28ab34846")]
+[assembly: Guid("50a16717-00a1-43b7-b7ca-2584a13f292f")]
 
 // Version information for an assembly consists of the following four values:
 //
